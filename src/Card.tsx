9ea40c448ec7ee6e8105712e7d6a7b1b0f76c1ba
Reyna DeLogé <reyna.deloge@gmail.com>
2019-11-17 18:28:14 +0100

    compress images, trigger browser download of images

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Card.css';
 
 export interface CardProps {
@@ -11,6 +11,11 @@ export interface CardProps {
 
 const Card: React.FC<CardProps> = ({id, image, visible, onClick, disabled}) => {
   const backgroundImage = visible ? image : '/images/card-front.jpg';
+  useEffect(() => {
+    const img = new Image();
+    img.src = image;
+  });
+
   return (
     <div className={`card ${visible ? 'visible' : ''} ${disabled ? 'disabled' : ''}`}
       onClick={() => visible || disabled ? null : onClick(id)}
